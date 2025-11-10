77655167aa26a9cecba0e03877e149d19654306d
Ruslan S. <ruslan.drrasl@yandex.ru>
2025-11-10 12:07:56 +0300

    Rev.8 - Добавил actuator и логирование через Аспект для shopping-store

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/commerce/shopping/store/controller/ShoppingStoreController.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/commerce/shopping/store/controller/ShoppingStoreController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.commerce.shopping.store.config.Loggable;
 import ru.yandex.practicum.commerce.contract.shopping.store.ShoppingStoreOperations;
 import ru.yandex.practicum.commerce.dto.shopping.store.ProductCategory;
 import ru.yandex.practicum.commerce.dto.shopping.store.ProductDto;
@@ -24,50 +25,55 @@ public class ShoppingStoreController implements ShoppingStoreOperations {
 
     @Override
     @GetMapping
+    @Loggable
     public Page<ProductDto> getProducts(
             @RequestParam ProductCategory category,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size,
             @RequestParam(required = false) String sort) {
 
-        log.debug("Requested products for category: {}, page: {}, size: {}, sort: {}",
-                category, page, size, sort);
+//        log.debug("Requested products for category: {}, page: {}, size: {}, sort: {}",
+//                category, page, size, sort);
         Page<ProductDto> productDtos = shoppingStoreService.getProducts(category, page, size, sort);
-        log.debug("Got products {} for category: {}", productDtos, category);
+//        log.debug("Got products {} for category: {}", productDtos, category);
         return productDtos;
     }
 
     @Override
     @PutMapping
+    @Loggable
     public ProductDto createNewProduct(@RequestBody ProductDto productDto) {
-        log.debug("Received new product to create: {}", productDto);
+//        log.debug("Received new product to create: {}", productDto);
         ProductDto newProduct = shoppingStoreService.createProduct(productDto);
-        log.debug("Return created product : {}", newProduct);
+//        log.debug("Return created product : {}", newProduct);
         return newProduct;
     }
 
     @Override
     @PostMapping
+    @Loggable
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
-        log.debug("Updating product with id: {}", productDto.getProductId());
+//        log.debug("Updating product with id: {}", productDto.getProductId());
         ProductDto updatedProduct = shoppingStoreService.updateProduct(productDto);
-        log.debug("Return updated product : {}", updatedProduct);
+//        log.debug("Return updated product : {}", updatedProduct);
         return updatedProduct;
     }
 
     @Override
     @PostMapping("/removeProductFromStore")
+    @Loggable
     public boolean removeProductFromStore(@RequestBody UUID productId) {
-        log.debug("Removing product from store with id: {}", productId);
+//        log.debug("Removing product from store with id: {}", productId);
         return shoppingStoreService.removeProductFromStore(productId);
     }
 
     @Override
     @PostMapping("/quantityState")
+    @Loggable
     public boolean setProductQuantityState(
             @RequestParam UUID productId,
             @RequestParam QuantityState quantityState) {
-        log.debug("Setting quantity state for product: {}, state: {}", productId, quantityState);
+//        log.debug("Setting quantity state for product: {}, state: {}", productId, quantityState);
         SetProductQuantityStateRequest request = SetProductQuantityStateRequest.builder()
                 .productId(productId)
                 .quantityState(quantityState)
@@ -77,10 +83,11 @@ public class ShoppingStoreController implements ShoppingStoreOperations {
 
     @Override
     @GetMapping("/{productId}")
+    @Loggable
     public ProductDto getProduct(@PathVariable UUID productId) {
-        log.debug("Getting product with id: {}", productId);
+//        log.debug("Getting product with id: {}", productId);
         ProductDto productDto = shoppingStoreService.getProduct(productId);
-        log.debug("Return product : {}", productDto);
+//        log.debug("Return product : {}", productDto);
         return productDto;
     }
 }
