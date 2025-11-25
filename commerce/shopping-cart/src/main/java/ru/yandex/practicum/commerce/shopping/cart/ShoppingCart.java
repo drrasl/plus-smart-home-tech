3b1cdebb4ec5db00b5a7c653e1c137d65d654d0d
Ruslan S. <ruslan.drrasl@yandex.ru>
2025-11-25 17:00:57 +0300

    Rev.7 - обавил кеширование для ShoppingStore

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/shopping/cart/ShoppingCart.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/shopping/cart/ShoppingCart.java
@@ -8,7 +8,7 @@ import ru.yandex.practicum.commerce.contract.warehouse.WarehouseClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableFeignClients(clients = WarehouseClient.class)
+@EnableFeignClients(basePackages = "ru.yandex.practicum.commerce.contract")
 public class ShoppingCart {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCart.class, args);
